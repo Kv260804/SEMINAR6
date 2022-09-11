6c2b249e4afa6c160036ac10cddf0bee974fc4c5
Kostya <kostya.v2608@gmail.com>
2022-09-11 22:25:56 +0300

    Добавили дз 6 семинара

---
+++ Sem6ДЗ/Num41/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// void CreateArray(int[] array)
+// {
+//     for (i = 0; i < M; i++)
+// {
+//     Console.WriteLine("Введите элемент массива: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+     
+// }
+// i = 0;
+// for (i = 0; i < M; i++)
+// {
+//     Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
+
+
+// void CheckArray(int[] array)
+// {
+//    for(i = 0; i < M; i++)
+// {
+//     if(array[i] > 0)
+//     {
+//         count = count + 1;
+//     }
+//     else
+//     { 
+//         count = count;
+//     }
+// } 
+// }
+
+Console.WriteLine("Введите количество чисел массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [M];
+int i = 0;
+int count = 0;
+for (i = 0; i < M; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+     
+}
+i = 0;
+for (i = 0; i < M; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for(i = 0; i < M; i++)
+{
+    if(array[i] > 0)
+    {
+        count = count + 1;
+    }
+    else
+    { 
+        count = count;
+    }
+}
+Console.WriteLine("Количество чисел больше 0, которые ввёл пользователь: " + count);
